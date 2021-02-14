ad77b2488bebb73cc624f8435dfdd484009340be
MabiGermano <mabi.germano.e@gmail.com>
2021-02-14 01:07:53 -0300

    socket connection on server

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,36 @@ const socketIO = require('socket.io');
 const routes = require('./routes');
 
 const app = express();
-app.use(routes);
 app.use(cors);
+app.use(routes);
 
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = socketIO(
+    server,
+    {
+        cors: {
+            origin: 'http://localhost:3000',
+            methods: ['GET', 'POST']
+        }
+    }
+);
 
-app.listen(3333);
+let interval;
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => getApiAndEmit(socket), 1000);
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+        clearInterval(interval);
+    });
+});
+
+const getApiAndEmit = socket => {
+    const response = new Date();
+    socket.emit('FromAPI', response);
+};
+
+server.listen(3333, () => console.log(`Listening on port 3333`));
