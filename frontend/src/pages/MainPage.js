acdc08485bd6f37f512e716eabffb6abf43b753f
MabiGermano <mabi.germano.e@gmail.com>
2021-02-15 21:51:22 -0300

    introducing youtube videos

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,8 +1,43 @@
 
-function MainPage () {
-    return(
+function MainPage() {
+    const player;
+
+    function onYouTubePlayerAPIReady() {
+        player = new YT.Player('ytplayer', {
+            height: '360',
+            width: '640',
+            videoId: 'tcYodQoapMg',
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+
+    function onPlayerReady(event) {
+        //event.target.playVideo();
+    }
+
+    function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PAUSED) {
+            const time = getFormattedTime(player.getCurrentTime());
+            document.querySelector('#info').innerHTML = `Foi passado para o tempo: ${time}`;
+        }
+    }
+
+    function stopVideo() {
+        player.stopVideo();
+    }
+
+    function getFormattedTime(timeInSeconds) {
+        return timeInSeconds < 60 ?
+            `0:${Math.trunc(timeInSeconds)}` :
+            `${Math.trunc(timeInSeconds / 60)}:${Math.trunc(timeInSeconds % 60)}`;
+    }
+    return (
         <main>
-            <h1>isso aqui é a main page</h1>
+            <div id="ytplayer"></div>
+            <div id="info"></div>
         </main>
     );
 }
