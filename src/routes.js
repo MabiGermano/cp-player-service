d7f38a62894377f45a5abcd102562e76ee00adc2
MabiGermano <beatriz.germano@nubank.com.br>
2022-01-16 14:27:59 -0300

    notificate clients when new video was added + adding user into a room

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const serverFunction = require("./server")
+
 
 router.get('/', (req, res) => {
     res.send({ response: 'I am alive' }).status(200);
   });
+
+router.post('/notify/new-video', (req, res) => {
+  const {room} = req.body;
+  serverFunction.emitEvent(room.identifier, room.playlist)
+  res.send({ response: 'I am alive' }).status(200);
+})
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,36 +1,43 @@
-const express = require('express');
-const cors = require('cors');
-const http = require('http');
-const socketIO = require('socket.io');
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const socketIO = require("socket.io");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 const app = express();
-app.use(cors);
+app.use(cors({ origin: process.env.CORS_ORIGIN || "*" }));
+app.use(express.json());
 app.use(routes);
 
 const server = http.createServer(app);
-const io = socketIO(
-    server, {
-        cors: {
-            origin: '*',
-            methods: ['GET', 'POST']
-        }
-    }
-);
-
-io.on('connection', (socket) => {
-    console.log('New client connected');
-
-    socket.on('PlayerAction', (action) => {
-        console.log('ação feita');
-        socket.broadcast.emit('SetTune', action);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  const room = socket.request._query.id;
+  console.log(room);
+  socket.join(room);
+
+  socket.on("PlayerAction", (data) => {
+    const {room, action} = data;
+    console.log("ação feita");
+    socket.to(room).emit("SetTune", action);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+exports.emitEvent = function (roomIdentifier, data) {  
+  io.to(roomIdentifier).emit("UpdatePlaylist", data);
+};
 
 server.listen(3333, () => console.log(`Listening on port 3333`));
